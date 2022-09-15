75551ddea830c0a6b2bc67030ad1c05652dde6cb
Daniel Hu <hdmdhr@gmail.com>
2022-09-15 14:08:52 -0600

    Initial Commit

---
+++ SwifteeCode/SwifteeCode.h
@@ -0,0 +1,18 @@
+//
+//  SwifteeCode.h
+//  SwifteeCode
+//
+//  Created by Daniel Hu on 2022-09-15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwifteeCode.
+FOUNDATION_EXPORT double SwifteeCodeVersionNumber;
+
+//! Project version string for SwifteeCode.
+FOUNDATION_EXPORT const unsigned char SwifteeCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwifteeCode/PublicHeader.h>
+
+
